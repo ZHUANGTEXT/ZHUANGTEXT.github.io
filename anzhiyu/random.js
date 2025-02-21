99f9572c48e1bde04edb60597924e7739eef6767
Zhuangmoumou <zmm156300@163.com>
2025-02-21 22:34:40 +0800

    Site updated: 2025-02-21 22:34:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/21/cfjs/","2025/02/21/hello-world/"];function toRandomPost(){
+var posts=["cfjs/","hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
